aea4e0bb3a082433f3955a8cc744bf4f99e8a902
changoo <changoo@Changoo-PC>
2016-12-03 00:42:55 +0900

    commit

--- src/main/java/kr/co/changoo/controller/SubjectCotroller.java
+++ src/main/java/kr/co/changoo/controller/SubjectCotroller.java
@@ -22,59 +22,62 @@ public class SubjectCotroller {
 	public void setCreditService(SubjectService subjectService) {
 		this.subjectService = subjectService;
 	}
-	
+
 	public SubjectService getCreditService() {
 		return subjectService;
 	}
 
-	
-	@RequestMapping(value="/showSubjectsOfSemester")
-	public String showSubjectsOfSemester(Model model,Subject subject){
-		int year=subject.getYear();
-		int semester=subject.getSemester();
-		
-		List<Subject> subjects=subjectService.getSubjectsBySemseter(year, semester);
-		model.addAttribute("subjects",subjects);
-		
+	@RequestMapping(value = "/showSubjectsOfSemester")
+	public String showSubjectsOfSemester(Model model, Subject subject) {
+		int year = subject.getYear();
+		int semester = subject.getSemester();
+
+		List<Subject> subjects = subjectService.getSubjectsBySemseter(year, semester);
+		model.addAttribute("subjects", subjects);
+
 		return "showSubjectsOfSemester";
 	}
-	
+
 	@RequestMapping(value = "/showCreditsOfSemester")
 	public String showCreditsOfSemester(Model model) {
-		int enterYear=2011;
-		int currentYear=2016;
-		
-		ArrayList<Integer[]> creditsList=new ArrayList<>();
-		
-		for(Integer year=enterYear; year<=currentYear;year++)
-			for(Integer semester=1; semester<=2; semester++){
-				if(subjectService.getCreditsBySemetser(year, semester)!=null){
-					int credits=subjectService.getCreditsBySemetser(year, semester);					
-					creditsList.add(new Integer[]{year,semester,credits});				
+		int enterYear = 2011;
+		int currentYear = 2016;
+
+		ArrayList<Integer[]> creditsList = new ArrayList<>();
+
+		for (Integer year = enterYear; year <= currentYear; year++)
+			for (Integer semester = 1; semester <= 2; semester++) {
+				if (subjectService.getCreditsBySemetser(year, semester) != null) {
+					int credits = subjectService.getCreditsBySemetser(year, semester);
+					creditsList.add(new Integer[] { year, semester, credits });
 				}
 			}
-		
-		model.addAttribute("creditsList",creditsList);
+
+		model.addAttribute("creditsList", creditsList);
 		return "showCreditsOfSemester";
 	}
 
 	@RequestMapping(value = "/showCreditsOfDivision")
 	public String showCreditsOfDivision(Model model) {
 
-		Integer credits_kyopil=subjectService.getCreditsByDivision("교필");
-		Integer credits_todae=subjectService.getCreditsByDivision("토대");
-		Integer credits_injae=subjectService.getCreditsByDivision("인재");
-		Integer credits_jungi=subjectService.getCreditsByDivision("전기");
-		Integer credits_junzi=subjectService.getCreditsByDivision("전지");
-		Integer credits_junsun=subjectService.getCreditsByDivision("전선");
-		
-		model.addAttribute("credits_kyopil",credits_kyopil);
-		model.addAttribute("credits_todae",credits_todae);
-		model.addAttribute("credits_injae",credits_injae);
-		model.addAttribute("credits_jungi",credits_jungi);
-		model.addAttribute("credits_junzi",credits_junzi);
-		model.addAttribute("credits_junsun",credits_junsun);
-		
+		Integer credits_kyopil = subjectService.getCreditsByDivision("교필");
+		Integer credits_todae = subjectService.getCreditsByDivision("토대");
+		Integer credits_injae = subjectService.getCreditsByDivision("인재");
+		Integer credits_zayoul = subjectService.getCreditsByDivision("자율");
+		Integer credits_jungi = subjectService.getCreditsByDivision("전기");
+		Integer credits_junzi = subjectService.getCreditsByDivision("전지");
+		Integer credits_junsun = subjectService.getCreditsByDivision("전선");
+		Integer allCredits = credits_kyopil + credits_todae + credits_injae + credits_zayoul + credits_jungi
+				+ credits_junzi + credits_junsun;
+		model.addAttribute("credits_kyopil", credits_kyopil);
+		model.addAttribute("credits_todae", credits_todae);
+		model.addAttribute("credits_injae", credits_injae);
+		model.addAttribute("credits_zayoul", credits_zayoul);
+		model.addAttribute("credits_jungi", credits_jungi);
+		model.addAttribute("credits_junzi", credits_junzi);
+		model.addAttribute("credits_junsun", credits_junsun);
+		model.addAttribute("allCredits", allCredits);
+
 		return "showCreditsOfDivision";
 	}
 
--- src/main/webapp/WEB-INF/views/showCreditsOfDivision.jsp
+++ src/main/webapp/WEB-INF/views/showCreditsOfDivision.jsp
@@ -11,7 +11,7 @@
 <body>
 
 	구분별 이수 학점 조회
-	
+
 	<table width="460" cellspacing="1" cellpadding="3" border="0"
 		bgcolor="#737373" valign="top">
 		<tr>
@@ -19,18 +19,23 @@
 			<td bgcolor="#D0D8FF" width="40" align="center">교필</td>
 			<td bgcolor="#D0D8FF" width="50" align="center">토대</td>
 			<td bgcolor="#D0D8FF" width="50" align="center">인재</td>
+			<td bgcolor="#D0D8FF" width="50" align="center">자율</td>
 			<td bgcolor="#D0D8FF" width="50" align="center">전기</td>
 			<td bgcolor="#D0D8FF" width="50" align="center">전지</td>
 			<td bgcolor="#D0D8FF" width="50" align="center">전선</td>
+			<td bgcolor="#D0D8FF" width="50" align="center">총학점</td>
 		</tr>
 		<tr>
 
 			<td bgcolor="#F5F5F5" align="center">${credits_kyopil}</td>
 			<td bgcolor="#F5F5F5" align="center">${credits_todae}</td>
 			<td bgcolor="#F5F5F5" align="center">${credits_injae}</td>
+			<td bgcolor="#F5F5F5" align="center">${credits_zayoul}</td>
+
 			<td bgcolor="#F5F5F5" align="center">${credits_jungi}</td>
 			<td bgcolor="#F5F5F5" align="center">${credits_junzi}</td>
 			<td bgcolor="#F5F5F5" align="center">${credits_junsun}</td>
+			<td bgcolor="#F5F5F5" align="center">${allCredits}</td>
 		</tr>
 
 	</table>
